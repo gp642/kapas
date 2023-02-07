e7b8b086a75345201468a243768a16612f64c95c
Rajat Gupta <rajat.gupta@franconnect.com>
2023-02-07 23:07:41 +0530

    Initial application

---
+++ src/main/java/com/example/kapas/KapasApplication.java
@@ -0,0 +1,13 @@
+package com.example.kapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapasApplication.class, args);
+	}
+
+}
